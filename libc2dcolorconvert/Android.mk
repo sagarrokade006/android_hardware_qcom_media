b2e34c3db8ce5297f43a44ae7ac01e443f9fd6ed
Arne Coucheron <arco68@gmail.com>
2018-10-21 00:27:58 +0200

    media: Use generated kernel headers
    Change-Id: I2f848353e2e8d1ba0b233c1d0b271e0595ff9a79
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- libc2dcolorconvert/Android.mk
+++ libc2dcolorconvert/Android.mk
@@ -10,18 +10,14 @@ LOCAL_CFLAGS += -Wno-error
 LOCAL_C_INCLUDES := \
     $(TARGET_OUT_HEADERS)/qcom/display
 
-LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-
-LOCAL_CFLAGS += -Wno-error
-
 LOCAL_SHARED_LIBRARIES := libutils liblog libdl
+
+LOCAL_HEADER_LIBRARIES := generated_kernel_headers
 
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_MODULE := libc2dcolorconvert
 
 LOCAL_VENDOR_MODULE := true
-
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 include $(BUILD_SHARED_LIBRARY)
