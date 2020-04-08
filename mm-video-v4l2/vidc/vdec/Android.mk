4ca78df3fd86fb4e7fe31431a135e318be436881
Sagarrokade006 <sagarrokade006@gmail.com>
2020-04-08 09:22:10 +0000

    Revert "Adapt path for HAL"
    This reverts commit 3a3c7a4af35abdb295943cbb76ef662aa69eab4b.

--- mm-video-v4l2/vidc/vdec/Android.mk
+++ mm-video-v4l2/vidc/vdec/Android.mk
@@ -61,7 +61,7 @@ include $(CLEAR_VARS)
 # Common Includes
 libmm-vdec-inc          := $(LOCAL_PATH)/inc
 libmm-vdec-inc          += $(OMX_VIDEO_PATH)/vidc/common/inc
-libmm-vdec-inc          += hardware/qcom/media/mm-core/inc
+libmm-vdec-inc          += $(TOP)/$(call project-path-for,qcom-media)/mm-core/inc
 libmm-vdec-inc          += $(TARGET_OUT_HEADERS)/qcom/display
 libmm-vdec-inc          += $(TARGET_OUT_HEADERS)/adreno
 libmm-vdec-inc          += $(TOP)/frameworks/native/include/media/openmax
@@ -69,7 +69,7 @@ libmm-vdec-inc          += $(TOP)/frameworks/native/include/media/hardware
 libmm-vdec-inc          += frameworks/native/libs/nativewindow/include/
 libmm-vdec-inc          += frameworks/native/libs/arect/include/
 libmm-vdec-inc          += frameworks/native/libs/nativebase/include
-libmm-vdec-inc      	+= hardware/qcom/media/libc2dcolorconvert
+libmm-vdec-inc      	+= $(TOP)/$(call project-path-for,qcom-media)/libc2dcolorconvert
 libmm-vdec-inc      	+= $(TOP)/frameworks/av/include/media/stagefright
 ifeq ($(call is-platform-sdk-version-at-least,27),true) #O_MR1
 libmm-vdec-inc          += $(TOP)/frameworks/native/libs/nativewindow/include
@@ -80,7 +80,7 @@ libmm-vdec-inc      	+= $(TARGET_OUT_HEADERS)/mm-video/SwVdec
 
 ifeq ($(PLATFORM_SDK_VERSION), 18)  #JB_MR2
 libmm-vdec-def += -DANDROID_JELLYBEAN_MR2=1
-libmm-vdec-inc += hardware/qcom/media/libstagefrighthw
+libmm-vdec-inc += $(TOP)/$(call project-path-for,qcom-media)/libstagefrighthw
 endif
 
 ifeq ($(call is-platform-sdk-version-at-least, 19),true)
